011b424fac41781e6e0ba20bc20ce1c21bded374
oscarlvp <oscarlvp@gmail.com>
2019-02-19 11:56:01 +0100

    Better handling timestamped reports

--- unstable-mutation-score/run-100-times.sh
+++ unstable-mutation-score/run-100-times.sh
@@ -2,5 +2,6 @@
 
 for run in {1..100}
 do
-  mvn -Pquality org.pitest:pitest-maven:mutationCoverage@pitest-check -Dverbose=true -DoutputFormats=JSON,METHODS > target/$run.output.txt
+  mvn -Pquality org.pitest:pitest-maven:mutationCoverage@pitest-check -Dverbose=true -DoutputFormats=JSON,METHODS -DreportsDirectory=target/pit-reports/`date +"%s"` -DtimestampedReports=false> target/$run.output.txt
+  sleep 1
 done
